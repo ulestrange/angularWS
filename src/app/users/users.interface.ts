3e69976e0cf0f5258959e79e3b96d0134d8635f4
Una <una.lestrange@atu.ie>
2025-10-20 15:46:22 +0100

    ready for the service

---
+++ src/app/users/users.interface.ts
@@ -0,0 +1,9 @@
+export interface UsersInterface {
+    id?: string;
+    name: string;
+    phonenumber: string;
+    email: string;
+    dob?: Date;
+    dateJoined?: Date,
+    lastUpdated?: Date
+}

1e87db7b07b98addb2cbd886e19e94d0ad3011ef
Una <una.lestrange@atu.ie>
2025-10-21 21:13:23 +0100

    added retry

---
+++ src/app/shared/notification-service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar} from '@angluar/material/snack-bar'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificationService {
+  
+}

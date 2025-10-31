9eed47b8610bfc280b63edf3fc3db96c2e1e3886
Una <una.lestrange@atu.ie>
2025-10-31 16:38:49 +0000

    added home component and some simple routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Users } from './users/users';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Home},
+    {path: 'home', redirectTo: '/'},
+    {path: 'user-list', component: Users},
+    ];
+
